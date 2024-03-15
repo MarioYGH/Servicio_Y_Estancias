f1e12f370012b34fa6bfe53dc76749642f6d7792
MarioYGH <113196424+MarioYGH@users.noreply.github.com>
2024-03-15 10:15:17 -0600

    Create Compuerta_NAND.vhd

---
+++ EstanciasCFATA/FPGA/Compuerta_NAND.vhd
@@ -0,0 +1,20 @@
+-- Compuerta NAND, <= es asignación directa y utilizamos la plabra reservada NAND para indicar que A y B realizaran esta operación 
+
+library IEEE;
+use ieee.std_logic_1164.all -- Libreria estandar 
+
+-- Se le llama black box, caja negra a la parte donde se declaran las varibles
+-- Descripción en caja negra 
+  
+entity Compuerta_NAND is
+	port (
+	   A, B : in std_logic; -- Entradas simples
+     F:     out std_logic; -- Salida simple
+	);
+	end Compuerta_NAND;
+
+  -- Descripción del circuito 
+  architecture simple of Compuerta_NAND is 
+    begin 
+      F <= A NAND B; -- Compuerta NAND 
+    end simple;
