d181c261430c8930d742bdc0570a7d5091e2e25a
MarioYGH <113196424+MarioYGH@users.noreply.github.com>
2024-04-05 11:01:23 -0600

    Create Demux_1_2.vhd

---
+++ EstanciasCFATA/FPGA/Demux_1_2.vhd
@@ -0,0 +1,18 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+--Blackbox
+
+entity Demux_1_2 is 
+port(
+	X : in std_logic; --Entradas
+	S : in std_logic; --Seleccion
+	Y : out std_logic_vector(1 downto 0) --Salida
+	);
+--Descripción circuito 
+
+architecture simple of Demux_1_2 is 
+	begin 
+	  Y(0) <= X when (S='0') else '0'; --Asignacion condicional	
+    Y(1) <= X when (S='1') else '0'; --Asignacion condicional	
+	end simple; 
