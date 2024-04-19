18146d1a30b0612f41f4724992add51cbab1b10f
MarioYGH <113196424+MarioYGH@users.noreply.github.com>
2024-04-19 12:40:41 -0600

    Update BCD_7_segmentos.vhd

--- EstanciasCFATA/FPGA/BCD_7_segmentos.vhd
+++ EstanciasCFATA/FPGA/BCD_7_segmentos.vhd
@@ -6,7 +6,7 @@ use IEEE.std_logic_1164.all;
 entity BCD_7_segmentos is 
 port(
 	B : in  std_logic_vector(3 downto 0); --Entrada BCD
-	S : out std_logic_vector(7 downto 1); --Salida 7 segmentos 
+	S : out std_logic_vector(7 downto 1) --Salida 7 segmentos 
 	); 
 end BCD_7_segmentos;
 
