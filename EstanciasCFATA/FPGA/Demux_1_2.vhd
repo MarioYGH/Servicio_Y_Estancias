e6a128565274a2a64e42b35da14cd513bf71e577
MarioYGH <113196424+MarioYGH@users.noreply.github.com>
2024-04-19 12:47:18 -0600

    Update Demux_1_2.vhd

--- EstanciasCFATA/FPGA/Demux_1_2.vhd
+++ EstanciasCFATA/FPGA/Demux_1_2.vhd
@@ -9,10 +9,11 @@ port(
 	S : in std_logic; --Seleccion
 	Y : out std_logic_vector(1 downto 0) --Salida
 	);
+  end Demux_1_2;
 --Descripción circuito 
 
 architecture simple of Demux_1_2 is 
 	begin 
 	  Y(0) <= X when (S='0') else '0'; --Asignacion condicional	
-    Y(1) <= X when (S='1') else '0'; --Asignacion condicional	
+          Y(1) <= X when (S='1') else '0'; --Asignacion condicional	
 	end simple; 
