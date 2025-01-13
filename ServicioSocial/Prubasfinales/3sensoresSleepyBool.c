276ba70772054682b0a5331abc47eb09e0d70772
MarioYGH <113196424+MarioYGH@users.noreply.github.com>
2025-01-13 09:55:53 -0600

    Update 3sensoresSleepyBool.c

--- ServicioSocial/Prubasfinales/3sensoresSleepyBool.c
+++ ServicioSocial/Prubasfinales/3sensoresSleepyBool.c
@@ -10,6 +10,8 @@
 #include "esp_adc/adc_oneshot.h"
 #include "driver/gpio.h"
 #include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 #include "esp_sleep.h"
 
 #define ADC1_CHAN7 ADC_CHANNEL_7 /* GPIO 35 for Sensor 1 */
@@ -17,7 +19,6 @@
 #define ADC1_CHAN5 ADC_CHANNEL_5 /* GPIO 33 for Sensor 3 */
 #define ADC_ATTEN ADC_ATTEN_DB_11
 #define N_SENSORS 3
-#define BUF_SIZE 100
 
 #define GPIO_SENSOR1_STATE GPIO_NUM_25 /* GPIO pin for Sensor 1 state */
 #define GPIO_SENSOR2_STATE GPIO_NUM_26 /* GPIO pin for Sensor 2 state */
@@ -90,6 +91,13 @@ void calculate_distance(int sensor_index, float voltage) {
                                coeff[5];
 }
 
+void read_sensor(int sensor_index, adc_channel_t channel) {
+    get_adc_value(adc1_handle, channel, &adc_raws[sensor_index]);
+    adc_voltages[sensor_index] = adc_raws[sensor_index] * 3.3 / 4095.0;
+    calculate_distance(sensor_index, adc_voltages[sensor_index]);
+    ESP_LOGI(TAG, "Sensor %d Distance: %.2f mm", sensor_index + 1, distances[sensor_index]);
+}
+
 void app_main(void){
     adc_initialize(); /* ADC initialization */
     gpio_initialize(); /* GPIO initialization */
@@ -97,19 +105,24 @@ void app_main(void){
     while(1){
         update_sensor_states(); /* Update sensor states from GPIO */
 
-        for (int i = 0; i < N_SENSORS; i++) {
-            if (sensor_states[i]) { // Check if the sensor should be read
-                /* Read Sensor */
-                get_adc_value(adc1_handle, (adc_channel_t)(ADC1_CHAN7 - i * 3), &adc_raws[i]);
-                adc_voltages[i] = adc_raws[i] * 3.3 / 4095.0;
-                calculate_distance(i, adc_voltages[i]);
-                ESP_LOGI(TAG, "Sensor %d Distance: %.2f mm", i + 1, distances[i]);
-            } else {
-                ESP_LOGI(TAG, "Sensor %d is disabled", i + 1);
-            }
+        if (sensor_states[0]) {
+            read_sensor(0, ADC1_CHAN7);
+        } else {
+            ESP_LOGI(TAG, "Sensor 1 is disabled");
         }
 
-        /* Sleep the ESP32 */
+        if (sensor_states[1]) {
+            read_sensor(1, ADC1_CHAN4);
+        } else {
+            ESP_LOGI(TAG, "Sensor 2 is disabled");
+        }
+
+        if (sensor_states[2]) {
+            read_sensor(2, ADC1_CHAN5);
+        } else {
+            ESP_LOGI(TAG, "Sensor 3 is disabled");
+        }
+
         ESP_LOGI(TAG, "Entering deep sleep for 5 minutes...");
         esp_deep_sleep(300000000); // 5 minutes in microseconds
         // For 12 hours, change to: esp_deep_sleep(43200000000);
